1d1f2ddcf8a457de669d61a1cdcc29dc89b49aa3
Breno Nascimento <satsukebare23@gmail.com>
2025-09-12 20:39:10 -0300

    10 - Configurando rotas privadas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function App() {
-  return (
-    <View>
-      <Text className="text-purple-base">Olá mundo!!</Text>
-    </View>
-  );
+  const userData = true;
+
+  if (!userData) {
+    return <Redirect href={"/(private)"} />;
+  }
+
+  return <Redirect href={"/login"} />;
 }
