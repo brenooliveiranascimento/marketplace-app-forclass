225d59330a49bca6d52ddeed7b92907d39b4339a
Breno Nascimento <satsukebare23@gmail.com>
2025-09-22 01:27:11 -0300

    Configurando store

--- src/viewModels/Register/useRegister.viewModel.ts
+++ src/viewModels/Register/useRegister.viewModel.ts
@@ -2,9 +2,11 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { RegisterFormData, registerScheme } from "./register.scheme";
 import { useRegisterMutation } from "../../shared/queries/auth/use-register.mutation";
+import { useUserStore } from "../../shared/store/user-store";
 
 export const useRegisterViewModel = () => {
   const userRegisterMutation = useRegisterMutation();
+  const { setSession, user } = useUserStore();
 
   const {
     control,
@@ -13,18 +15,27 @@ export const useRegisterViewModel = () => {
   } = useForm<RegisterFormData>({
     resolver: yupResolver(registerScheme),
     defaultValues: {
-      phone: "",
-      confirmPassword: "",
-      email: "",
-      name: "",
-      password: "",
+      phone: "12312312312",
+      confirmPassword: "123123123",
+      email: "teste123123teste@gmail.com",
+      name: "teste1",
+      password: "123123123",
     },
   });
 
   const onSubmit = handleSubmit(async (userData) => {
     const { confirmPassword, ...registerData } = userData;
-    await userRegisterMutation.mutateAsync(registerData);
+    const mutationResponse = await userRegisterMutation.mutateAsync(
+      registerData
+    );
+    setSession({
+      refreshToken: mutationResponse.refreshTokern,
+      token: mutationResponse.token,
+      user: mutationResponse.user,
+    });
   });
+
+  console.log(user);
 
   return {
     control,
