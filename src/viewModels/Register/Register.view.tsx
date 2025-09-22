11c5b3134906bd3cd9390292a31ec501e0ae01bf
Breno Nascimento <satsukebare23@gmail.com>
2025-09-22 00:19:53 -0300

    17 teste

--- src/viewModels/Register/Register.view.tsx
+++ src/viewModels/Register/Register.view.tsx
@@ -1,15 +1,16 @@
 import { FC, useState } from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { useRegisterViewModel } from "./useRegister.viewModel";
 
-export const RegisterView: FC<
-  ReturnType<typeof useRegisterViewModel>
-> = ({
-  
+export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
+  onSubmit,
 }) => {
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Register</Text>
+      <TouchableOpacity onPress={onSubmit}>
+        <Text>Registrar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- src/viewModels/Register/useRegister.viewModel.ts
+++ src/viewModels/Register/useRegister.viewModel.ts
@@ -1,8 +1,11 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { RegisterFormData, registerScheme } from "./register.scheme";
+import { useRegisterMutation } from "../../shared/queries/auth/use-register.mutation";
 
 export const useRegisterViewModel = () => {
+  const userRegisterMutation = useRegisterMutation();
+
   const {
     control,
     handleSubmit,
@@ -18,7 +21,10 @@ export const useRegisterViewModel = () => {
     },
   });
 
-  const onSubmit = handleSubmit(({}) => {});
+  const onSubmit = handleSubmit(async (userData) => {
+    const { confirmPassword, ...registerData } = userData;
+    await userRegisterMutation.mutateAsync(registerData);
+  });
 
   return {
     control,
