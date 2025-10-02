790acc8605753006d85bee797a59945d24fc5ee7
Breno Nascimento <satsukebare23@gmail.com>
2025-10-02 15:56:31 -0300

    31

--- src/viewModels/Login/Login.view.tsx
+++ src/viewModels/Login/Login.view.tsx
@@ -2,20 +2,23 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { AuthFormHeader } from "../../shared/components/AuthFormHeader";
 import { AppInput } from "../../shared/components/AppInput";
 import { router } from "expo-router";
+import { KeyboardContainer } from "../../shared/components/KeyboardContainer";
 
 export const LoginView = () => {
   return (
-    <View className="items-center justify-center flex-1">
-      <AuthFormHeader
-        subTitle="Informe seu e-mail e senha para entrar"
-        title="Acesse sua conta"
-      />
+    <KeyboardContainer>
+      <View className="flex-1 items-center justify-center px-[40px]">
+        <AuthFormHeader
+          subTitle="Informe seu e-mail e senha para entrar"
+          title="Acesse sua conta"
+        />
 
-      <AppInput />
+        <AppInput />
 
-      <TouchableOpacity onPress={() => router.push("/register")}>
-        <Text>Registro</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity onPress={() => router.push("/register")}>
+          <Text>Registro</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardContainer>
   );
 };
--- src/viewModels/Register/Register.view.tsx
+++ src/viewModels/Register/Register.view.tsx
@@ -1,36 +1,69 @@
 import { FC, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { useRegisterViewModel } from "./useRegister.viewModel";
 import { AppInput } from "../../shared/components/AppInput";
 import { Controller } from "react-hook-form";
 import { AppInputController } from "../../shared/components/AppInputController";
 import { AuthFormHeader } from "../../shared/components/AuthFormHeader";
 import { router } from "expo-router";
+import { KeyboardContainer } from "../../shared/components/KeyboardContainer";
 
 export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
   onSubmit,
   control,
 }) => {
   return (
-    <View className="flex-1  justify-center">
-      <AuthFormHeader
-        title="Crie sua conta"
-        subTitle="Informe os seus dados pessoais e de acesso"
-      />
-      <AppInputController
-        leftIcon="mail-outline"
-        label="E-MAIL"
-        control={control}
-        name="email"
-      />
+    <KeyboardContainer>
+      <ScrollView className="flex-1 px-[40px]">
+        <AuthFormHeader
+          title="Crie sua conta"
+          subTitle="Informe os seus dados pessoais e de acesso"
+        />
+        <AppInputController
+          leftIcon="person-outline"
+          label="NOME"
+          control={control}
+          name="name"
+        />
 
-      <TouchableOpacity onPress={onSubmit}>
-        <Text>Registrar</Text>
-      </TouchableOpacity>
+        <AppInputController
+          leftIcon="mail-outline"
+          label="E-MAIL"
+          control={control}
+          name="email"
+        />
 
-      <TouchableOpacity onPress={() => router.push("/login")}>
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
+        <AppInputController
+          leftIcon="call-outline"
+          label="TELEFONE"
+          control={control}
+          name="phone"
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="SENHA"
+          control={control}
+          name="password"
+          secureTextEntry
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="CONFIRMAR SENHA"
+          control={control}
+          name="confirmPassword"
+          secureTextEntry
+        />
+
+        <TouchableOpacity onPress={onSubmit}>
+          <Text>Registrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push("/login")}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardContainer>
   );
 };
