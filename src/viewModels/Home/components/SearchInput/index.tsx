d79a5f5679aaf8c7412a6d75e10213607d2b097c
Breno Nascimento <satsukebare23@gmail.com>
2025-10-26 19:18:03 -0300

    59

---
+++ src/viewModels/Home/components/SearchInput/index.tsx
@@ -0,0 +1,29 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { AppInput } from "../../../../shared/components/AppInput";
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from "../../../../styles/colors";
+
+export const SearchInput = () => {
+  return (
+    <View className="mb-3 mt-6">
+      <Text className="text-2xl font-bold mt-6">Explore Produtos</Text>
+      <View className="flex-row">
+        <View className="flex-1">
+          <AppInput
+            leftIcon="search"
+            returnKeyType="search"
+            className="text-lg flex-1"
+          />
+        </View>
+
+        <TouchableOpacity className="ml-5 mt-6 items-center justify-center rounded-lg border-[1px] h-[48px] w-[48px] border-purple-base">
+          <Ionicons
+            name="filter-outline"
+            size={24}
+            color={colors["purple-base"]}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
