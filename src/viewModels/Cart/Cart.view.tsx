9fe85a4e435efcc6ad53b36602fad74036a0f843
Breno Nascimento <satsukebare23@gmail.com>
2025-11-09 22:10:25 -0300

    106

---
+++ src/viewModels/Cart/Cart.view.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { FlatList, Text, View } from "react-native";
+import { useCartViewModel } from "./useCart.viewModel";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ProductCartCard } from "./components/ProductCartCard";
+
+export const CartView: FC<ReturnType<typeof useCartViewModel>> = ({
+  products,
+}) => {
+  return (
+    <SafeAreaView>
+      <FlatList
+        contentContainerClassName="px-6"
+        data={products}
+        renderItem={({ item }) => <ProductCartCard product={item} />}
+        keyExtractor={({ id }) => `product-cart-id-${id}`}
+      />
+    </SafeAreaView>
+  );
+};
---
+++ src/viewModels/Cart/useCart.viewModel.ts
@@ -0,0 +1,7 @@
+import { useCartStore } from "../../shared/store/cart-store";
+
+export const useCartViewModel = () => {
+  const { products } = useCartStore();
+
+  return { products };
+};
