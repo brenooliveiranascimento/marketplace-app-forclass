b303c2d9ac86ef83039fc7c49975d24dbe52a574
Breno Nascimento <satsukebare23@gmail.com>
2025-11-11 18:08:31 -0300

    113

---
+++ src/shared/queries/credit-cards/use-create-credit-card.mutation.ts
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCreditCard } from "../../services/credit-card.service";
+import { CreateCreditCardRequestParams } from "../../interfaces/http/crete-credit-card";
+import { Toast } from "toastify-react-native";
+
+export const useCreateCreditCardMutation = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (creditCardData: CreateCreditCardRequestParams) =>
+      createCreditCard(creditCardData),
+    onSuccess: (response) => {
+      Toast.success(response.message ?? "Cartão criado com sucesso");
+      queryClient.cancelQueries({
+        queryKey: ["credit-cards"],
+      });
+    },
+  });
+
+  return mutation;
+};
