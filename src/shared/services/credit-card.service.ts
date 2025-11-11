4e4c63bd4d20a33f8ea80e221b89f4868302a2b4
Breno Nascimento <satsukebare23@gmail.com>
2025-11-11 17:59:35 -0300

    112

---
+++ src/shared/services/credit-card.service.ts
@@ -0,0 +1,10 @@
+import { marketPlaceApiClient } from "../api/market-place";
+import { GetCreditCardsResponse } from "../interfaces/http/credit-card";
+
+export const getCreditCards = async () => {
+  const { data } = await marketPlaceApiClient.get<GetCreditCardsResponse[]>(
+    "/credit-cards"
+  );
+
+  return data;
+};
